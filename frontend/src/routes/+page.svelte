dd0cf5803effeb00b58d133e1b97295ff898ba92
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-11-09 22:05:56 +0530

    feat: config proper favicon and metahead

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>Quibble - Delve into real conversations.</title>
+</svelte:head>
+
+<h1 class="text-2xl font-black text-primary">Quibble</h1>
+<p>Delve into real conversations.</p>
