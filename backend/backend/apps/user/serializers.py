8f116c53b40b78b8a094248f3b398ef2a22f1dce
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-11-12 18:34:22 +0530

    chore(backend): format and remove dead code

--- backend/backend/apps/user/serializers.py
+++ backend/backend/apps/user/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import User, Profile
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'date_joined')
         read_only_fields = ('date_joined',)
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/backend/apps/user/views.py
+++ backend/backend/apps/user/views.py
@@ -3,9 +3,11 @@ from rest_framework import viewsets
 from .serializers import ProfileSerializer, UserSerializer
 from .models import Profile, User
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
