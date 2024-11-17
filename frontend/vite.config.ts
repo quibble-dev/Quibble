6215bc595a9dfe1a72eb630870eceeaa118780b4
daaniissh <pkdanish329@gmail.com>
2024-11-17 15:23:19 +0530

    chore: set up alias for  to point to /src/lib in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	resolve: {
+    alias: {
+      $lib: '/src/lib',
+      
+    }
+  },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
