66eacbdce5ee0194cd8134d0fc20cf7657b7f838
daaniissh <pkdanish329@gmail.com>
2024-11-17 15:22:44 +0530

    feat: add <Header/> component to the main page layout

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,9 @@
+<script>
+	import Header from "$lib/components/Header/Header.svelte";
+
+</script>
 <svelte:head>
 	<title>Quibble - Delve into real conversations.</title>
 </svelte:head>
 
-<h1 class="text-2xl font-black text-primary">Quibble</h1>
-<p>Delve into real conversations!.</p>
+<Header/>
