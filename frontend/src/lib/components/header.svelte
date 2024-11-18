df04edff3d8be5d716c32eb35063c304a9768191
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-11-18 12:10:36 +0530

    fix: header searchbar and borders

--- frontend/src/lib/components/header.svelte
+++ frontend/src/lib/components/header.svelte
@@ -6,7 +6,7 @@
 	let is_authenticated = $state(true);
 </script>
 
-<header class="flex items-center justify-between bg-base-200 p-2.5 px-4">
+<header class="flex items-center justify-between border-b border-neutral bg-base-200 p-2.5 px-4">
 	<!-- quibble logo -->
 	<a href="/" aria-label="Quibble Home" class="flex items-center gap-2">
 		<QuibbleLogo class="size-7" />
@@ -28,11 +28,13 @@
 				<span class="font-bold">All</span>
 			</a>
 		</div>
-		<label class="input input-bordered flex h-10 w-96 items-center bg-transparent px-3">
+		<label
+			class="input input-bordered flex h-10 w-96 items-center bg-transparent px-3 transition-colors focus-within:bg-base-100"
+		>
 			<coreproject-shape-search class="size-5"></coreproject-shape-search>
 			<input
 				type="text"
-				class="grow border-none font-medium focus:ring-0"
+				class="grow border-none text-sm font-semibold focus:ring-0"
 				placeholder="Search..."
 			/>
 		</label>
