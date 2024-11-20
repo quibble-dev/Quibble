880542ff56dff7372d1ce3d5efcd39393135dce5
daaniissh <pkdanish329@gmail.com>
2024-11-20 12:56:38 +0530

    feat: rightPanel

---
+++ frontend/src/lib/components/RightPanel/RightPost.svelte
@@ -0,0 +1,41 @@
+<script lang="ts" >
+	type postProps = {
+		title: string;
+		userImg?: string;
+		postImg?: string;
+		username: string;
+		upvotes: number;
+		comments: number;
+	};
+	import { formatNumber } from '$lib/utils/formatNumber';
+	import UserListItem from '../User/UserListItem.svelte';
+	let {comments,title,upvotes,username,userImg,postImg}: postProps = $props();
+  let votes = formatNumber(upvotes)
+  let cmnts = formatNumber(comments)
+</script>
+
+<div class="flex gap-2">
+	<!--others  -->
+	<div class="">
+		<UserListItem
+			name={username}
+			img={userImg}
+		/>
+		<div class="cursor-pointer">
+			<h1
+				class="text-info font-sans text-xl font-extrabold transition-all duration-1000 hover:underline"
+			>
+			{title}
+			</h1>
+		</div>
+		<div class="flex gap-2">
+			<p class="text-base-content text-sm font-medium">{votes} upvotes</p>
+			<span>•</span>
+			<p class="text-base-content text-sm font-medium">{cmnts} comments</p>
+		</div>
+	</div>
+	<!-- image div -->
+	<div class="flex w-24 object-contain flex-col items-center justify-center">
+		<img class="rounded-xl" src={postImg} alt="" />
+	</div>
+</div>
---
+++ frontend/src/lib/components/RightPanel/rightPanel.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Post from './RightPost.svelte';
+</script>
+
+<div class="fixed right-0 h-screen w-80">
+	<h2 class="text-info py-4 font-sans font-semibold">Recent Posts</h2>
+	<!-- recent posts -->
+	<div class="flex flex-col gap-6">
+		<!-- Post -->
+		<Post comments={500} upvotes={1000} postImg="https://avatars.githubusercontent.com/u/114811070?v=4" title="I love moonlightgrace, she is mine" userImg="https://i.pinimg.com/736x/53/c2/9d/53c29d1baf7aa6d98206235b4021ee87.jpg" username="gayi" />
+		<Post comments={54000} upvotes={22300} title="any one want my beer" postImg="https://i.pinimg.com/736x/e4/3e/1e/e43e1e3db0f2dc79caef437717e01cb5.jpg" userImg="https://i.pinimg.com/736x/70/8e/32/708e32ee69a19b625290653acb582840.jpg"  username="madison beer" />
+
+	</div>
+</div>
