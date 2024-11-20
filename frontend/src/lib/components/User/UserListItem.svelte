6f7dfdc0151c097c21463535fa17ae1283427dd4
daaniissh <pkdanish329@gmail.com>
2024-11-20 12:55:43 +0530

    feat: enhanced UserListItem component for better user display

--- frontend/src/lib/components/User/UserListItem.svelte
+++ frontend/src/lib/components/User/UserListItem.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	type User = {
 		name: string;
-		stared: boolean;
-		img: string;
+		stared?: boolean;
+		img?: string;
 	};
 	import Star from '$lib/components/icons/StarIcon.svelte';
 	import StarFill from '$lib/components/icons/StarFill.svelte';
@@ -12,7 +12,7 @@
 
 <div class="mb-3 flex w-full cursor-pointer items-center justify-between gap-2">
 	<div class="flex gap-2">
-		{#if img?.length == 0}
+		{#if !img}
 			<div class="avatar placeholder">
 				<div class="text-neutral-content w-[30px] rounded-full bg-lime-500">
 					<span class="text-[16px] font-bold capitalize text-black">{name.slice(0, 1)}</span>
@@ -21,19 +21,20 @@
 		{:else}
 			<div class="avatar">
 				<div class="w-[30px] rounded-full">
-					<img
-						alt=""
-						src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-					/>
+					<img alt="" src={img} />
 				</div>
 			</div>
 		{/if}
 
 		<h1>q/{name}</h1>
 	</div>
-	{#if stared}
-		<StarFill />
-	{:else}
-		<Star />
+	{#if stared !== undefined}
+		<div>
+			{#if stared}
+				<StarFill />
+			{:else}
+				<Star />
+			{/if}
+		</div>
 	{/if}
 </div>
