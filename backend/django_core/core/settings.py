101ff65bf8b32de4c51d0138b02c2e61d28cf050
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-11-21 00:33:50 +0530

    chore: add

--- backend/django_core/core/settings.py
+++ backend/django_core/core/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'ilovedjango_core')
+SECRET_KEY = os.getenv('SECRET_KEY', 'ilovequibble')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG', 1))
