c2eb99486b7b375ff0fca24c6217f86fe77fed16
ilovemoon <moonlitgrace.gaia@gmail.com>
2024-11-21 12:08:31 +0530

    feat: integrate tests for API and update structure (#50)

---
+++ backend/tests/api/user/test_auth.py
@@ -0,0 +1,41 @@
+import pytest
+
+
+@pytest.mark.django_db
+class TestAuth:
+    def test_authenticated_request(self, auth_api_client, user, user_profile):
+        response = auth_api_client.get('/api/user/me/')
+
+        assert response.status_code == 200
+        data = response.data
+        assert data['user']['email'] == user.email
+        assert data['id'] == user_profile.id
+        assert data['username'] == user_profile.username
+
+    def test_unauthenticated_request(self, api_client):
+        response = api_client.get('/api/user/me/')
+
+        assert response.status_code == 401
+
+    def test_login(self, api_client, user, token):
+        payload = dict(email=user.email, password='testpass')
+
+        response = api_client.post('/api/user/auth/login/', payload, format='json')
+
+        assert response.status_code == 200
+        data = response.data
+        assert data['token'] == token.key
+
+    def test_logout(self, auth_api_client):
+        response = auth_api_client.post('/api/user/auth/logout/')
+
+        assert response.status_code == 200
+
+    def test_register(self, api_client):
+        payload = dict(email='another@test.com', password='test')
+
+        response = api_client.post('/api/user/auth/register/', payload, format='json')
+
+        assert response.status_code == 201
+        data = response.data
+        assert data['email'] == payload['email']
