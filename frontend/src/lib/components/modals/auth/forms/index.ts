ed32748036a9e1504b0df93ad8b4b9c5b022a777
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-11-24 22:14:42 +0530

    refactor: better auth forms typing

---
+++ frontend/src/lib/components/modals/auth/forms/index.ts
@@ -0,0 +1,7 @@
+const forms = {
+	login: import('./login.svelte'),
+	profile_select: import('./profile_select.svelte'),
+	profile_create: import('./profile_create.svelte')
+};
+
+export default forms;
