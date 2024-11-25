3d84b650df18faa4f0293626766504c2f97d6cc5
meowsocute <meowsocute111@gmail.com>
2024-11-25 20:43:31 +0530

    feat: posts layout selector drop down integration.

--- frontend/src/lib/components/pages/home/posts_header.svelte
+++ frontend/src/lib/components/pages/home/posts_header.svelte
@@ -5,6 +5,7 @@
 	import NewIcon from '$lib/components/icons/new.svelte';
 	import RocketIcon from '$lib/components/icons/rocket.svelte';
 	import TopIcon from '$lib/components/icons/top.svelte';
+	import { cn } from '$lib/functions/classnames';
 
 	let active_mapping = $state<{
 		filter: keyof typeof mapping.filters;
@@ -22,10 +23,25 @@
 			top: { icon: TopIcon, href: '/top' }
 		},
 		view: {
-			compact: { icon: CompactIcon, onclick: () => {} },
-			card: { icon: CardIcon, onclick: () => {} }
+			compact: {
+				icon: CompactIcon,
+				onclick: () => {
+					active_mapping.view = 'compact';
+				}
+			},
+			card: {
+				icon: CardIcon,
+				onclick: () => {
+					active_mapping.view = 'card';
+				}
+			}
 		}
 	};
+
+	let active_view_icon = $derived(mapping.view[active_mapping.view]);
+
+	console.log(mapping);
+	console.log(mapping.view[active_mapping.view].icon);
 </script>
 
 <div class="flex items-center justify-between">
@@ -39,25 +55,33 @@
 					<span class="text-sm font-bold capitalize">{key}</span>
 				</a>
 				{#if is_active}
-					<div class="h-0.5 w-5 rounded-full bg-primary"></div>
+					<div class="bg-primary h-0.5 w-5 rounded-full"></div>
 				{/if}
 			</div>
 		{/each}
 	</div>
 	<div class="flex gap-3">
-		<span class="text-sm font-bold">View:</span>
-		{#each Object.entries(mapping.view) as [key, item]}
-			{@const is_active = active_mapping.view === key}
-
-			<div class="flex flex-col items-center gap-1">
-				<button onclick={item.onclick} aria-label="{key} view" class="flex items-center gap-2">
-					<item.icon class="size-4" />
-					<span class="text-sm font-bold capitalize">{key}</span>
-				</button>
-				{#if is_active}
-					<div class="h-0.5 w-5 rounded-full bg-primary"></div>
-				{/if}
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn btn-sm hover:bg-neutral m-1 bg-transparent">
+				<active_view_icon.icon class="stroke-primary"/>
+				<span class="text-sm font-bold capitalize">{active_mapping.view}</span>
 			</div>
-		{/each}
+			<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+			<ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-32 p-2 shadow gap-1">
+				{#each Object.entries(mapping.view) as [key, item]}
+					{@const is_active = active_mapping.view === key}
+					<li>
+						<button
+							onclick={item.onclick}
+							aria-label="{key} view"
+							class="flex items-center gap-2 p-2"
+						>
+							<item.icon class={cn(is_active ? 'stroke-primary' : 'stroke-neutral-content', 'size-4')} />
+							<span class={cn(is_active && 'text-primary',"text-sm font-bold capitalize")}>{key}</span>
+						</button>
+					</li>
+				{/each}
+			</ul>
+		</div>
 	</div>
 </div>
