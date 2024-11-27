1d0c846a844074f1bc94201ba38716ac8e747065
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-11-27 09:20:45 +0530

    chore: add JSDoc for apiFetch function

--- frontend/src/lib/utils/api.ts
+++ frontend/src/lib/utils/api.ts
@@ -1,5 +1,24 @@
 import { PUBLIC_API_URL } from '$env/static/public';
 
+/**
+ * A utility function to make API requests.
+ *
+ * @template T - The type of the response data.
+ * @param endpoint - The API endpoint to call.
+ * @param init_options - Optional request configuration, eg: body, method, etc.
+ *
+ * @returns A promise that resolves to the parsed data of type T.
+ *
+ * @throws { Error } Throws an error if the response if not ok.
+ *
+ * @example
+ * ```ts
+ * // Example of getting a token with type.
+ * const { token } = await apiFetch<{ token: string }>('v1/user/auth/login/', {
+ *   body: JSON.stringify({ email, password })
+ * });
+ * ```
+ */
 export async function apiFetch<T>(
 	endpoint: string,
 	{ body, ...init_options }: RequestInit = {}
@@ -11,6 +30,7 @@ export async function apiFetch<T>(
 			'Content-Type': 'application/json',
 			...init_options?.headers
 		},
+		// attach body if body is provided (for POST)
 		...(body && { body })
 	};
 
