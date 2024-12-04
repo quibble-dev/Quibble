d7872a9318ef20c5609cc51c9524406e098d616a
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-04 17:29:50 +0530

    refactor: change views and viewsets import pattern

--- backend/apps/user/api/urls.py
+++ backend/apps/user/api/urls.py
@@ -1,24 +1,25 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from . import views, viewsets
+from .views import LoginAPIView, LogoutAPIView, MeAPIView, RegisterAPIView
+from .viewsets import MyProfilesViewSet, ProfileViewSet, UserViewSet
 
 # main router for users/ and profiles/
 router = DefaultRouter()
 
-router.register(r'', viewsets.UserViewSet)
-router.register(r'profiles', viewsets.ProfileViewSet)
+router.register(r'', UserViewSet)
+router.register(r'profiles', ProfileViewSet)
 # profiles of requested user
-router.register(r'me/profiles', viewsets.MyProfilesViewSet, basename='me-profile')
+router.register(r'me/profiles', MyProfilesViewSet, basename='me-profile')
 
 # fmt: off
 urlpatterns = [
     # auth endpoints
-    path('login/', views.LoginAPIView.as_view(), name='login'),
-    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
-    path('register/', views.RegisterAPIView.as_view(), name='register'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('register/', RegisterAPIView.as_view(), name='register'),
     # user view of requested user
-    path('me/', views.MeAPIView.as_view(), name='me'),
+    path('me/', MeAPIView.as_view(), name='me'),
 ]
 # fmt: on
 
