38e4d9d3a55062f60aa312939eb9769cc9871d5d
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-08 20:09:14 +0530

    fix: validation for quiblet unique name api response

--- backend/apps/quiblet/api/v1/serializers.py
+++ backend/apps/quiblet/api/v1/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
 from apps.quiblet.models import Quiblet
@@ -7,3 +8,11 @@ class QuibletSerializer(ModelSerializer):
     class Meta:
         model = Quiblet
         fields = '__all__'
+
+    def validate_name(self, name):
+        if Quiblet.objects.filter(name__iexact=name).exists():
+            raise ValidationError(
+                f"Quiblet with name {name} already exists (case-insensitive)."
+            )
+
+        return name
