37c2b0d5f07197406be0a8d4ce4a0532148f8e68
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-09 09:37:20 +0530

    refactor: nodejs script to generate openapi schema

---
+++ frontend/scripts/generate_schema.js
@@ -0,0 +1,24 @@
+import { exec } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const api_url = process.env.PUBLIC_API_URL;
+
+if (!api_url) {
+	console.error('error: PUBLIC_API_URL is not defined in your .env file.');
+	process.exit(0);
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const output_path = path.resolve(__dirname, '../src/lib/types/schema.d.ts');
+
+const command = `openapi-typescript ${api_url}/api/v1/schema/ -o ${output_path}`;
+
+exec(command, (error, stdout, _) => {
+	if (error) {
+		console.log(`error: ${error}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+});
