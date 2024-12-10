52e18f1857a9da5b14260cddda3e747b027e01e0
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-10 11:31:00 +0530

    refactor: drf spectacular dynamic dict generation
    simplified drf speculator ENUMS_NAME_OVERRIDES with dict comprehension to avoid repetition and better readability

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -108,17 +108,20 @@ SPECTACULAR_SETTINGS = {
     # integrate with drf-standardized-errors
     # https://drf-standardized-errors.readthedocs.io/en/latest/openapi.html
     'ENUM_NAME_OVERRIDES': {
-        'ValidationErrorEnum': 'drf_standardized_errors.openapi_serializers.ValidationErrorEnum.choices',
-        'ClientErrorEnum': 'drf_standardized_errors.openapi_serializers.ClientErrorEnum.choices',
-        'ServerErrorEnum': 'drf_standardized_errors.openapi_serializers.ServerErrorEnum.choices',
-        'ErrorCode401Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode401Enum.choices',
-        'ErrorCode403Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode403Enum.choices',
-        'ErrorCode404Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode404Enum.choices',
-        'ErrorCode405Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode405Enum.choices',
-        'ErrorCode406Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode406Enum.choices',
-        'ErrorCode415Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode415Enum.choices',
-        'ErrorCode429Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode429Enum.choices',
-        'ErrorCode500Enum': 'drf_standardized_errors.openapi_serializers.ErrorCode500Enum.choices',
+        key: f"drf_standardized_errors.openapi_serializers.{key}.choices"
+        for key in [
+            'ValidationErrorEnum',
+            'ClientErrorEnum',
+            'ServerErrorEnum',
+            'ErrorCode401Enum',
+            'ErrorCode403Enum',
+            'ErrorCode404Enum',
+            'ErrorCode405Enum',
+            'ErrorCode406Enum',
+            'ErrorCode415Enum',
+            'ErrorCode429Enum',
+            'ErrorCode500Enum',
+        ]
     },
     'POSTPROCESSING_HOOKS': [
         'drf_standardized_errors.openapi_hooks.postprocess_schema_enums',
