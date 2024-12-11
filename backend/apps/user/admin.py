2c8587e2f4d68656c5bf8e60fd1088a58631b13c
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-11 08:42:34 +0530

    chore: update customuseradmin

--- backend/apps/user/admin.py
+++ backend/apps/user/admin.py
@@ -2,37 +2,27 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from .forms import CustomUserAdminForm
 from .models import CustomUser
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    # form = CustomUserAdminForm
-    add_form = CustomUserAdminForm
-
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
         (
-            _('permissions'),
+            _('Permissions'),
             {
-                'fields': ('is_active', 'is_staff', 'is_superuser'),
-            },  # , 'groups', 'user_permissions')},
-        ),
-        (_('important dates'), {'fields': ('date_joined',)}),
-    )
-
-    add_fieldsets = (
-        (
-            None,
-            {
-                'classes': ('wide',),
-                'fields': ('email', 'password', 'is_active', 'is_staff', 'is_superuser'),
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
             },
         ),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
 
-    readonly_fields = ('date_joined',)
-    list_display = ('email', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
-    search_fields = ('email',)
-    ordering = ('email',)
+    readonly_fields = ('date_joined', 'last_login')
