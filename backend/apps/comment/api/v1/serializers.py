7d73fdd6ef72d3834acba5c919c9f2a710f726c5
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-13 23:37:57 +0530

    feat: comment serializer

---
+++ backend/apps/comment/api/v1/serializers.py
@@ -0,0 +1,30 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+
+from ...models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    path = serializers.CharField(required=False)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+    def create(self, validated_data):
+        data = {
+            'quibbler': validated_data.get('quibbler')
+            or self.context['request'].user_profile,
+            'content': validated_data['content'],
+        }
+
+        if path := validated_data.get('path'):
+            parent_instance = get_object_or_404(Comment, path__match=path)
+            comment_instance: Comment = Comment.objects.create_child(
+                parent=parent_instance, **data
+            )
+        else:
+            comment_instance: Comment = Comment.objects.create_child(**data)
+
+        comment_instance.save()
+        return comment_instance
