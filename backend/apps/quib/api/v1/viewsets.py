40a7e255c74b82d029a95f30ed6dc89200cbb2b5
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-14 12:44:05 +0530

    refactor: endpoint to create comment from quib

--- backend/apps/quib/api/v1/viewsets.py
+++ backend/apps/quib/api/v1/viewsets.py
@@ -1,4 +1,10 @@
-from rest_framework import viewsets
+from http import HTTPMethod
+
+from django.shortcuts import get_object_or_404
+from rest_framework import response, status, viewsets
+from rest_framework.decorators import action
+
+from apps.comment.api.v1.serializers import CommentSerializer
 
 from ...models import QuibModel
 from .serializers import QuibSerializer, QuibSlimSerializer
@@ -10,4 +16,19 @@ class QuibViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):  # pyright: ignore
         if self.action == 'list':
             return QuibSlimSerializer
+        # if custom action: 'comment'
+        if self.action == 'comment':
+            return CommentSerializer
         return QuibSerializer
+
+    @action(detail=True, methods=[HTTPMethod.POST])
+    def comment(self, request, pk=None):
+        quib_instance = get_object_or_404(QuibModel, pk=pk)
+
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        comment_instance = serializer.save()
+        quib_instance.comments.add(comment_instance)
+
+        return response.Response(status=status.HTTP_201_CREATED, data=serializer.data)
