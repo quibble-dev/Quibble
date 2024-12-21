79396473e3e76098a4d9f4e093da1532aecec037
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-21 20:29:52 +0530

    fix: ignore eslint rule for line

--- frontend/src/routes/(app)/q/+error.svelte
+++ frontend/src/routes/(app)/q/+error.svelte
@@ -11,7 +11,9 @@
   <img src={Quibble404} class="w-28" alt="" />
   <div class="flex flex-col">
     <h4 class="text-xl font-bold text-error">oh noh!!</h4>
+    <!-- eslint-disable svelte/no-at-html-tags -->
     <h5>{@html $page.error?.message}</h5>
+    <!-- eslint-enable svelte/no-at-html-tags -->
     <a href="/" aria-label="home" class="btn btn-primary btn-sm mt-4 w-max">
       <coreicons-shape-home class="size-4"></coreicons-shape-home>
       Back to home
