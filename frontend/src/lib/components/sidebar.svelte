b74052ea2d1f6f5ac626073bd5a6005bba798095
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-22 11:21:57 +0530

    chore: show fallback on sidebar_store

--- frontend/src/lib/components/sidebar.svelte
+++ frontend/src/lib/components/sidebar.svelte
@@ -34,16 +34,16 @@
       <span class="text-xs font-medium">Create Quiblet</span>
     </button>
   </div>
-  {#if sidebarStore.state.recent}
-    <div class="collapse gap-2 overflow-visible rounded-none">
-      <input type="checkbox" checked={true} class="peer h-max min-h-full w-full" />
-      <div
-        class="collapse-title flex h-max min-h-max items-center justify-between p-0 text-sm font-medium text-base-content/75 peer-checked:[&>coreicons-shape-chevron]:rotate-180"
-      >
-        Recent
-        <coreicons-shape-chevron class="size-4 transition-transform" variant="down"
-        ></coreicons-shape-chevron>
-      </div>
+  <div class="collapse gap-2 overflow-visible rounded-none">
+    <input type="checkbox" checked={true} class="peer h-max min-h-full w-full" />
+    <div
+      class="collapse-title flex h-max min-h-max items-center justify-between p-0 text-sm font-medium text-base-content/75 peer-checked:[&>coreicons-shape-chevron]:rotate-180"
+    >
+      Recent
+      <coreicons-shape-chevron class="size-4 transition-transform" variant="down"
+      ></coreicons-shape-chevron>
+    </div>
+    {#if sidebarStore.state.recent}
       <div class="collapse-content flex flex-col gap-2 !p-0">
         {#each sidebarStore.state.recent as quiblet}
           <div class="flex items-center gap-2">
@@ -58,8 +58,10 @@
           </div>
         {/each}
       </div>
-    </div>
-  {/if}
+    {:else}
+      <span class="text-sm font-medium">quite! check some.</span>
+    {/if}
+  </div>
   <div class="collapse gap-2 overflow-visible rounded-none">
     <input type="checkbox" checked={true} class="peer h-max min-h-full w-full" />
     <div
@@ -69,20 +71,24 @@
       <coreicons-shape-chevron class="size-4 transition-transform" variant="down"
       ></coreicons-shape-chevron>
     </div>
-    <div class="collapse-content flex flex-col gap-2 !p-0">
-      {#each your_communities as community}
-        <div class="flex items-center gap-2">
-          <a href="q/{community.name}" class="flex">
-            <Avatar src={community.avatar} />
-          </a>
-          <a href="q/{community.name}" class="text-sm font-medium">q/{community.name}</a>
-          <button class="ml-auto" aria-label="Star Community">
-            <coreicons-shape-star class="size-4" class:text-primary={community.starred}
-            ></coreicons-shape-star>
-          </button>
-        </div>
-      {/each}
-    </div>
+    {#if sidebarStore.state.your}
+      <div class="collapse-content flex flex-col gap-2 !p-0">
+        {#each your_communities as community}
+          <div class="flex items-center gap-2">
+            <a href="q/{community.name}" class="flex">
+              <Avatar src={community.avatar} />
+            </a>
+            <a href="q/{community.name}" class="text-sm font-medium">q/{community.name}</a>
+            <button class="ml-auto" aria-label="Star Community">
+              <coreicons-shape-star class="size-4" class:text-primary={community.starred}
+              ></coreicons-shape-star>
+            </button>
+          </div>
+        {/each}
+      </div>
+    {:else}
+      <span class="text-sm font-medium">join some.</span>
+    {/if}
   </div>
   <div class="collapse gap-2 overflow-visible rounded-none">
     <input type="checkbox" checked={true} class="peer h-max min-h-full w-full" />
