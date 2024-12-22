07b3a36e701cfe4b382ac594d22cc990b9e0291a
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-22 12:22:30 +0530

    chore: fix eslint and remove old unwanted mock data

--- frontend/src/lib/components/sidebar.svelte
+++ frontend/src/lib/components/sidebar.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import QuibbleIcon from '$lib/components/icons/logos/quibble.svelte';
   import Avatar from '$lib/components/ui/avatar.svelte';
-  import your_communities from '$lib/data/mock/your_communities.json';
   import { createSidebarStore } from '$lib/stores/sidebar.svelte';
 
   const sidebarStore = createSidebarStore();
