6475eca3181181b2ff60e0fc8885151cf90f7853
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-12-25 08:36:58 +0530

    refactor: utilize snippets for re-usable elements

--- frontend/src/lib/components/quib.svelte
+++ frontend/src/lib/components/quib.svelte
@@ -38,6 +38,69 @@
   }
 </script>
 
+{#snippet content_or_cover()}
+  {#if is_valid(quib.content)}
+    <p class="line-clamp-3 text-sm font-normal">
+      {quib.content}
+    </p>
+  {:else}
+    <img
+      class="cursor-pointer rounded-2xl inner-border inner-border-base-content/15"
+      src={quib.cover}
+      alt=""
+    />
+  {/if}
+{/snippet}
+
+{#snippet avatar_name_date()}
+  <div class="flex items-center gap-2">
+    <a
+      href="/{get_name()}"
+      class="relative flex items-center gap-2 hover:text-accent hover:underline"
+    >
+      <Avatar src={get_avatar()} class="size-6 rounded-full" />
+      <h3 class="text-xs font-semibold">{get_name()}</h3>
+    </a>
+    <coreicons-shape-circle variant="filled" class="size-0.5 text-base-content/75"
+    ></coreicons-shape-circle>
+    <span class="text-xs font-medium text-base-content/75"
+      >{new FormatDate(quib.created_at).timeAgo()}</span
+    >
+  </div>
+{/snippet}
+
+{#snippet vote_comment_share_more()}
+  <div class="flex items-center gap-2">
+    <button class="flex items-center gap-2" aria-label="upvote">
+      <coreicons-shape-thumbs variant="up" class="size-4" class:text-primary={is_upvoted}
+      ></coreicons-shape-thumbs>
+    </button>
+    <span class="text-sm font-medium">{readable(quib.upvotes?.length ?? 0)}</span>
+    <button class="flex items-center gap-2" aria-label="downvote">
+      <coreicons-shape-thumbs variant="down" class="size-4"></coreicons-shape-thumbs>
+    </button>
+  </div>
+  <button class="flex items-center gap-2">
+    <coreicons-shape-forum class="size-4"></coreicons-shape-forum>
+    <span class="text-sm font-medium">{readable(quib.comments?.length ?? 0)} comments</span>
+  </button>
+  <button class="flex items-center gap-2">
+    <coreicons-shape-share class="size-4"></coreicons-shape-share>
+    <span class="text-sm font-medium">Share</span>
+  </button>
+  <button class="ml-auto flex items-center gap-2" aria-label="more">
+    <coreicons-shape-more class="size-4 rotate-90"></coreicons-shape-more>
+  </button>
+{/snippet}
+
+{#snippet href_overlay()}
+  <a
+    href="/q/{quib.quiblet.name}/quibs/{quib.id}/{quib.slug}"
+    class="absolute inset-0"
+    aria-label={quib.title}
+  ></a>
+{/snippet}
+
 <div
   class={cn(
     viewStore.state === 'compact' && 'transition-colors hover:bg-base-200',
@@ -46,76 +109,17 @@
 >
   {#if viewStore.state === 'card'}
     <div class="relative flex flex-col gap-2 p-4 transition-colors hover:bg-base-200">
-      <a
-        href="/q/{quib.quiblet.name}/quibs/{quib.id}/{quib.slug}"
-        class="absolute inset-0"
-        aria-label={quib.title}
-      ></a>
-      <div class="flex items-center gap-2">
-        <a
-          href="/{get_name()}"
-          class="relative flex items-center gap-2 hover:text-accent hover:underline"
-        >
-          <Avatar src={get_avatar()} class="size-6 rounded-full" />
-          <h3 class="text-xs font-semibold">{get_name()}</h3>
-        </a>
-        <coreicons-shape-circle variant="filled" class="size-0.5 text-base-content/75"
-        ></coreicons-shape-circle>
-        <span class="text-xs font-medium text-base-content/75"
-          >{new FormatDate(quib.created_at).timeAgo()}</span
-        >
-      </div>
-
+      {@render href_overlay()}
+      {@render avatar_name_date()}
       <h2 class="text-xl font-bold text-info">{quib.title}</h2>
-
-      {#if is_valid(quib.content)}
-        <p class="line-clamp-3 text-sm font-normal" class:hidden={quib.cover}>
-          {quib.content}
-        </p>
-      {:else}
-        <img
-          class="cursor-pointer rounded-2xl inner-border inner-border-base-content/15"
-          src={quib.cover}
-          alt=""
-        />
-      {/if}
+      {@render content_or_cover()}
     </div>
-
     <div class="flex items-center gap-4 border-t border-neutral px-4 py-2.5">
-      <div class="flex items-center gap-2">
-        <button class="flex items-center gap-2" aria-label="upvote">
-          <coreicons-shape-thumbs
-            variant="up"
-            class="size-4"
-            class:text-primary={is_upvoted}
-          ></coreicons-shape-thumbs>
-        </button>
-        <span class="text-sm font-medium">{readable(quib.upvotes?.length ?? 0)}</span>
-        <button class="flex items-center gap-2" aria-label="downvote">
-          <coreicons-shape-thumbs variant="down" class="size-4"></coreicons-shape-thumbs>
-        </button>
-      </div>
-      <button class="flex items-center gap-2">
-        <coreicons-shape-forum class="size-4"></coreicons-shape-forum>
-        <span class="text-sm font-medium"
-          >{readable(quib.comments?.length ?? 0)} comments</span
-        >
-      </button>
-      <button class="flex items-center gap-2">
-        <coreicons-shape-share class="size-4"></coreicons-shape-share>
-        <span class="text-sm font-medium">Share</span>
-      </button>
-      <button class="ml-auto flex items-center gap-2" aria-label="more">
-        <coreicons-shape-more class="size-4 rotate-90"></coreicons-shape-more>
-      </button>
+      {@render vote_comment_share_more()}
     </div>
   {:else}
     <div class="flex-1 flex gap-4 p-4 p-4">
-      <a
-        href="/q/{quib.quiblet.name}/quibs/{quib.id}/{quib.slug}"
-        class="absolute inset-0"
-        aria-label={quib.title}
-      ></a>
+      {@render href_overlay()}
       <div
         class="cursor-pointer size-24 rounded-xl bg-cover bg-center inner-border
             inner-border-base-content/15 flex-shrink-0"
@@ -124,20 +128,7 @@
         style="background-image: url({quib.cover});"
       ></div>
       <div class="flex flex-col w-full justify-between">
-        <div class="flex items-center gap-2">
-          <a
-            href="/{get_name()}"
-            class="relative flex items-center gap-2 hover:text-accent hover:underline"
-          >
-            <Avatar src={get_avatar()} class="size-5 rounded-full" />
-            <h3 class="text-xs font-semibold">{get_name()}</h3>
-          </a>
-          <coreicons-shape-circle variant="filled" class="size-0.5 text-base-content/75"
-          ></coreicons-shape-circle>
-          <span class="text-xs font-medium text-base-content/75"
-            >{new FormatDate(quib.created_at).timeAgo()}</span
-          >
-        </div>
+        {@render avatar_name_date()}
         <h2 class="text-lg font-bold text-info">{quib.title}</h2>
         <div class="flex items-center gap-4 border-t border-neutral pt-2.5">
           <button
@@ -152,49 +143,13 @@
               <span class="text-sm font-medium">Expand</span>
             {/if}
           </button>
-          <div class="relative flex items-center gap-2">
-            <button class="flex items-center gap-2" aria-label="upvote">
-              <coreicons-shape-thumbs
-                variant="up"
-                class="size-4"
-                class:text-primary={is_upvoted}
-              ></coreicons-shape-thumbs>
-            </button>
-            <span class="text-sm font-medium">{readable(quib.upvotes?.length ?? 0)}</span>
-            <button class="flex items-center gap-2" aria-label="downvote">
-              <coreicons-shape-thumbs variant="down" class="size-4"
-              ></coreicons-shape-thumbs>
-            </button>
-          </div>
-          <button class="flex items-center gap-2">
-            <coreicons-shape-forum class="size-4"></coreicons-shape-forum>
-            <span class="text-sm font-medium"
-              >{readable(quib.comments?.length ?? 0)} comments</span
-            >
-          </button>
-          <button class="relative flex items-center gap-2">
-            <coreicons-shape-share class="size-4"></coreicons-shape-share>
-            <span class="text-sm font-medium">Share</span>
-          </button>
-          <button class="ml-auto flex items-center gap-2" aria-label="more">
-            <coreicons-shape-more class="size-4 rotate-90"></coreicons-shape-more>
-          </button>
+          {@render vote_comment_share_more()}
         </div>
       </div>
     </div>
     {#if is_expanded}
       <div class="p-4 pt-0">
-        {#if is_valid(quib.content)}
-          <p class="line-clamp-3 text-sm font-normal" class:hidden={quib.cover}>
-            {quib.content}
-          </p>
-        {:else}
-          <img
-            class="relative cursor-pointer rounded-2xl inner-border inner-border-base-content/15"
-            src={quib.cover}
-            alt=""
-          />
-        {/if}
+        {@render content_or_cover()}
       </div>
     {/if}
   {/if}
