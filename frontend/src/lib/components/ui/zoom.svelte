8ac4371c8c535875b5d5f754b07384579fa71908
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-05 10:47:55 +0530

    feat: custom zoom content

--- frontend/src/lib/components/ui/zoom.svelte
+++ frontend/src/lib/components/ui/zoom.svelte
@@ -5,11 +5,24 @@
 </script>
 
 <Zoom class_button="btn" zoom_margin={10}>
+  <!-- custom unzoom zoom buttons -->
   {#snippet icon_zoom()}
     <coreicons-shape-expand class="size-5"></coreicons-shape-expand>
   {/snippet}
   {#snippet icon_unzoom()}
     <coreicons-shape-shrink class="size-5"></coreicons-shape-shrink>
   {/snippet}
+  <!-- custom zoom content -->
+  {#snippet zoom_content({ img, button_unzoom, modal_state })}
+    <!-- zoomed image -->
+    {@render img()}
+    <div
+      class="opacity-0 transition-opacity"
+      class:opacity-100={modal_state === 'LOADING' || modal_state === 'LOADED'}
+    >
+      {@render button_unzoom()}
+    </div>
+  {/snippet}
+
   {@render children()}
 </Zoom>
