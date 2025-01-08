83a42a1bac3c63bcb5f2c91b1c86634e8afe2a1f
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-08 09:00:26 +0530

    refactor: serialize and de-serialize timestamp

--- frontend/src/lib/stores/recent_quibs.svelte.ts
+++ frontend/src/lib/stores/recent_quibs.svelte.ts
@@ -19,14 +19,24 @@ interface IRecentQuib extends Quib {
 
 const stored_recent_quibs = browser ? localStorage.getItem('recent_posts_store') : null;
 const parsed_stored_recent_quibs: IRecentQuib[] = stored_recent_quibs
-  ? JSON.parse(stored_recent_quibs)
+  ? // convert string to Date object
+    (JSON.parse(stored_recent_quibs) as IRecentQuib[]).map((q) => ({
+      ...q,
+      timestamp: new Date(q.timestamp)
+    }))
   : [];
 
 let recent_quibs_state = $state<IRecentQuib[]>(parsed_stored_recent_quibs);
 
 function sync_to_localstorage() {
   if (browser) {
-    localStorage.setItem('recent_posts_store', JSON.stringify(recent_quibs_state));
+    // convert Date object to string
+    localStorage.setItem(
+      'recent_posts_store',
+      JSON.stringify(
+        recent_quibs_state.map((q) => ({ ...q, timestamp: q.timestamp.toISOString() }))
+      )
+    );
   }
 }
 
