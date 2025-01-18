9012eb0ab98ba8c6310cc2e94aeaea21dbe4484a
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-18 23:07:58 +0530

    refactor: zod utility function

--- frontend/src/lib/utils/zod.ts
+++ frontend/src/lib/utils/zod.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
-export const zod_required_string = (name: string) => {
+export const zod_required_string = ({ field_name }: { field_name: string }) => {
   return z
     .string()
-    .min(1, { message: `${name} is required` })
+    .min(1, { message: `${field_name} is required` })
     .trim();
 };
--- frontend/src/lib/zod_schemas/auth.ts
+++ frontend/src/lib/zod_schemas/auth.ts
@@ -2,8 +2,12 @@ import { zod_required_string } from '$lib/utils/zod';
 import { z } from 'zod';
 
 export const auth_schema = z.object({
-  email: zod_required_string('Email').email({ message: 'Email is invalid' }),
-  password: zod_required_string('Password')
+  email: zod_required_string({ field_name: 'Email' }).email({
+    message: 'Email is invalid'
+  }),
+  password: zod_required_string({ field_name: 'Password' }).min(8, {
+    message: 'Password must contain atleast 8 chararcters'
+  })
 });
 
 export type AuthSchema = z.infer<typeof auth_schema>;
