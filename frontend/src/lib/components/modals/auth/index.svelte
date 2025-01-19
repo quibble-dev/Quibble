1363714fdb4650981866b07c4ad36e447556cc6b
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-19 16:34:37 +0530

    refactor: better history stack logic

--- frontend/src/lib/components/modals/auth/index.svelte
+++ frontend/src/lib/components/modals/auth/index.svelte
@@ -7,14 +7,7 @@
   let _form = $state<Forms>('join');
 
   let current_form = $derived(forms[_form]);
-  let prev_form_history = $state([
-    {
-      label: 'join',
-      form: 'join'
-    }
-  ]);
-
-  $inspect(prev_form_history);
+  let prev_form_history = $state<Forms[]>(['join']);
 
   const initial_forms_state = Object.fromEntries(
     Object.keys(forms).map((key) => [key, {}])
@@ -27,13 +20,29 @@
   }
 
   function goto_form(form: Forms) {
-    // push current form to prev_form_history
-    prev_form_history.push({
-      label: form.replace('_', ' '),
-      form: form
-    });
+    // if navigating to a form thats already in the history stack,
+    // truncate the stack upto the most recent occurance of that form
+    // (avoiding duplicate entiries)
+    const form_index = prev_form_history.findIndex((entry) => entry === form);
+    if (form_index > -1) {
+      // if exists
+      prev_form_history = prev_form_history.slice(0, form_index + 1);
+    } else {
+      // otherwise, push current form to prev_form_history
+      prev_form_history.push(form);
+    }
     // update form
     _form = form;
+  }
+
+  function handle_go_back() {
+    // remove current form from history stack
+    prev_form_history.pop();
+    // set current_form to previous form in history stack (the one before removed form)
+    const prev_form = prev_form_history.at(-1);
+    if (prev_form) {
+      _form = prev_form;
+    }
   }
 
   let dialog_element = $state<Nullable<HTMLDialogElement>>(null);
@@ -61,12 +70,12 @@
     {#if prev_form_history.length > 1}
       <div
         class="tooltip tooltip-right absolute left-2.5 top-2.5 flex before:capitalize"
-        data-tip={prev_form_history.at(-2)?.label}
+        data-tip={prev_form_history.at(-2)?.replace('_', ' ')}
       >
         <button
           class="btn btn-square btn-circle btn-ghost btn-sm"
           aria-label="Close modal"
-          onclick={() => goto_form(prev_form_history.at(-2)?.form as Forms)}
+          onclick={handle_go_back}
         >
           <coreicons-shape-arrow class="size-5" variant="left"></coreicons-shape-arrow>
         </button>
