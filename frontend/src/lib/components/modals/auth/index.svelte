258ddcfa33a6e6bc08937f4a4708b88b153f4ab6
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-19 13:51:23 +0530

    feat: prev_form history state

--- frontend/src/lib/components/modals/auth/index.svelte
+++ frontend/src/lib/components/modals/auth/index.svelte
@@ -7,7 +7,14 @@
   let _form = $state<Forms>('join');
 
   let current_form = $derived(forms[_form]);
-  let prev_form = $state<Nullable<Forms>>(null);
+  let prev_form_history = $state([
+    {
+      label: 'join',
+      form: 'join'
+    }
+  ]);
+
+  $inspect(prev_form_history);
 
   const initial_forms_state = Object.fromEntries(
     Object.keys(forms).map((key) => [key, {}])
@@ -20,7 +27,12 @@
   }
 
   function goto_form(form: Forms) {
-    prev_form = _form; // add to previous form state
+    // push current form to prev_form_history
+    prev_form_history.push({
+      label: form.replace('_', ' '),
+      form: form
+    });
+    // update form
     _form = form;
   }
 
@@ -46,15 +58,15 @@
     {#await current_form then Form}
       <Form.default {forms_state} {update_forms_state} {goto_form} />
     {/await}
-    {#if prev_form !== null}
+    {#if prev_form_history.length > 1}
       <div
-        class="tooltip tooltip-right absolute left-2.5 top-2.5 flex"
-        data-tip="Previous form"
+        class="tooltip tooltip-right absolute left-2.5 top-2.5 flex before:capitalize"
+        data-tip={prev_form_history.at(-2)?.label}
       >
         <button
           class="btn btn-square btn-circle btn-ghost btn-sm"
           aria-label="Close modal"
-          onclick={() => (_form = prev_form!)}
+          onclick={() => goto_form(prev_form_history.at(-2)?.form as Forms)}
         >
           <coreicons-shape-arrow class="size-5" variant="left"></coreicons-shape-arrow>
         </button>
