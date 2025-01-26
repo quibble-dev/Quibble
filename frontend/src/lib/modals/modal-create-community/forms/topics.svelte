9d73af742b613516b3fba19bb25df5ba98c4528d
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-26 23:05:28 +0530

    feat: toggle topic selection logic

--- frontend/src/lib/modals/modal-create-community/forms/topics.svelte
+++ frontend/src/lib/modals/modal-create-community/forms/topics.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import topics_data from '$lib/data/topics.json';
+  import topics from '$lib/data/topics.json';
+  import { cn } from '$lib/functions/classnames';
   import type { FormProps } from '../../types';
   import forms from '../forms';
 
@@ -9,6 +10,18 @@
   }>;
 
   let { forms_state }: FormProps<typeof forms> = $props();
+
+  let selected_topics = $state<Topics[number]['topics']>([]);
+
+  function handle_toggle_select_topic(topic: string) {
+    // remove topic if already selected
+    if (selected_topics.includes(topic)) {
+      selected_topics = selected_topics.filter((t) => t !== topic);
+    } else {
+      if (selected_topics.length >= 3) return;
+      selected_topics.push(topic);
+    }
+  }
 
   $effect(() => {
     console.log(forms_state);
@@ -20,15 +33,48 @@
     <h3 class="text-xl font-semibold text-info">Choose topics</h3>
     <p class="text-sm">Select up to 3 topics that represent what your community is about.</p>
   </div>
-  <div class="flex max-h-80 flex-col gap-4 overflow-scroll pr-2">
-    {#each topics_data as Topics as t}
+  <div class="flex flex-col gap-2">
+    <label class="input input-bordered relative flex h-10 items-center px-3">
+      <coreicons-shape-search class="size-5"></coreicons-shape-search>
+      <input
+        type="text"
+        class="grow border-none text-sm font-medium focus:ring-0"
+        placeholder="Filter topics..."
+      />
+    </label>
+    <div class="flex items-center gap-2">
+      <span class="text-sm font-medium">Topics {selected_topics.length}/3:</span>
+      {#each selected_topics as topic}
+        <button
+          class="btn btn-ghost btn-active btn-sm"
+          onclick={() => handle_toggle_select_topic(topic)}
+        >
+          {topic}
+          <coreicons-shape-x variant="circle" class="size-4"></coreicons-shape-x>
+        </button>
+      {/each}
+    </div>
+  </div>
+  <div class="flex max-h-64 flex-col gap-4 overflow-scroll pr-2">
+    {#each topics as Topics as t}
       <div class="flex flex-col gap-2">
         <span class="text-sm font-medium">{t.category}</span>
         <div class="flex flex-wrap items-center gap-2">
           {#each t.topics as topic}
-            <button class="btn btn-sm border-none bg-base-content/10 hover:btn-ghost"
-              >{topic}</button
+            {@const is_selected = selected_topics.includes(topic)}
+
+            <button
+              class={cn(
+                is_selected ? 'btn-info' : 'bg-base-content/10 hover:btn-ghost',
+                'btn btn-sm border-none'
+              )}
+              onclick={() => handle_toggle_select_topic(topic)}
             >
+              {topic}
+              {#if is_selected}
+                <coreicons-shape-x variant="circle" class="size-4"></coreicons-shape-x>
+              {/if}
+            </button>
           {/each}
         </div>
       </div>
