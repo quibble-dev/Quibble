97b7f06402142c1f589396e3d9b8f328af791342
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-27 22:47:49 +0530

    refactor: community creation type radio

--- frontend/src/lib/modals/modal-create-community/forms/index.ts
+++ frontend/src/lib/modals/modal-create-community/forms/index.ts
@@ -1,6 +1,7 @@
 const forms = {
   introduction: import('./introduction.svelte'),
-  topics: import('./topics.svelte')
+  topics: import('./topics.svelte'),
+  type: import('./type.svelte')
 };
 
 export default forms;
