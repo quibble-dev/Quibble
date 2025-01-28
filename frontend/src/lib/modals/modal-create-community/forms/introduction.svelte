6955d4a362ec370d51accdf37b5694c9d946ae92
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-28 10:47:30 +0530

    refactor: external avatar banner file inputs

--- frontend/src/lib/modals/modal-create-community/forms/introduction.svelte
+++ frontend/src/lib/modals/modal-create-community/forms/introduction.svelte
@@ -24,19 +24,19 @@
           name: string;
           description: string;
           avatar: string;
-          cover: string;
+          banner: string;
         }>;
       }
     ).data
   });
 
-  function handle_file_change(e: Event, type: 'avatar' | 'cover') {
+  function handle_file_change(e: Event, type: 'avatar' | 'banner') {
     const file = (e.target as HTMLInputElement).files?.[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
         if (type === 'avatar') introduction_data.avatar = String(reader.result);
-        if (type === 'cover') introduction_data.cover = String(reader.result);
+        if (type === 'banner') introduction_data.banner = String(reader.result);
       };
       reader.readAsDataURL(file);
     }
@@ -78,7 +78,7 @@
           <input
             bind:value={introduction_data.name}
             type="text"
-            class="grow border-none p-0 text-sm placeholder:opacity-50 focus:ring-0"
+            class="grow border-none p-0 text-sm placeholder:font-normal placeholder:opacity-50 focus:ring-0"
             placeholder="eg: quibble"
             maxlength={25}
             oninput={(e) => {
@@ -96,7 +96,7 @@
         <textarea
           bind:value={introduction_data.description}
           use:autosize
-          class="textarea textarea-bordered max-h-40 min-h-[5.5rem] bg-transparent leading-normal placeholder:opacity-50"
+          class="textarea textarea-bordered max-h-[15rem] min-h-[7.5rem] bg-transparent leading-normal placeholder:opacity-50"
           placeholder="Tell something nice about your community..."
           maxlength={255}
           oninput={handle_input}
@@ -106,53 +106,42 @@
         <ZodErrors {errors} />
       {/if}
     </div>
-    <div class="hidden w-72 flex-col md:flex">
-      <span class="py-1 text-sm">Preview</span>
+    <div class="hidden w-72 flex-col gap-2 md:flex">
+      <div class="grid grid-cols-2 gap-2">
+        <label class="form-control">
+          <div class="label py-1">
+            <span class="label-text">Avatar</span>
+          </div>
+          <input
+            type="file"
+            class="file-input file-input-bordered file-input-xs bg-transparent
+            file:border-none file:bg-base-100"
+            onchange={(e) => handle_file_change(e, 'avatar')}
+          />
+        </label>
+        <label class="form-control">
+          <div class="label py-1">
+            <span class="label-text">Banner</span>
+          </div>
+          <input
+            type="file"
+            class="file-input file-input-bordered file-input-xs bg-transparent
+            file:border-none file:bg-base-100"
+            onchange={(e) => handle_file_change(e, 'banner')}
+          />
+        </label>
+      </div>
       <div class="overflow-hidden rounded-2xl bg-base-100">
         <div
           class="flex h-10 bg-info bg-cover bg-center"
-          style="background-image: url({introduction_data.cover});"
-        >
-          <input
-            id="community-cover-upload"
-            type="file"
-            accept="image/*"
-            class="hidden"
-            onchange={(e) => handle_file_change(e, 'cover')}
-          />
-          <label
-            for="community-cover-upload"
-            class="btn btn-square btn-circle btn-sm m-1 ml-auto border-none bg-base-300/50"
-            aria-label="Upload community cover"
-          >
-            <coreicons-shape-edit variant="line-with-pencil" class="size-4"></coreicons-shape-edit>
-          </label>
-        </div>
+          style="background-image: url({introduction_data.banner});"
+        ></div>
         <div class="flex flex-col gap-4 p-4">
           <div class="flex items-center gap-4">
-            <div
-              class="group relative flex items-center justify-center overflow-hidden rounded-full"
-            >
-              <Avatar
-                src={introduction_data.avatar}
-                class="size-14 flex-shrink-0 !bg-base-content/15"
-              />
-              <input
-                id="community-avatar-upload"
-                type="file"
-                accept="image/*"
-                class="hidden"
-                onchange={(e) => handle_file_change(e, 'avatar')}
-              />
-              <label
-                for="community-avatar-upload"
-                class="btn btn-square btn-circle btn-sm absolute m-0 translate-y-12 transform opacity-100 transition-transform duration-300 group-hover:translate-y-0 group-hover:opacity-100"
-                aria-label="Upload community avatar"
-              >
-                <coreicons-shape-edit variant="line-with-pencil" class="size-4"
-                ></coreicons-shape-edit>
-              </label>
-            </div>
+            <Avatar
+              src={introduction_data.avatar}
+              class="size-14 flex-shrink-0 !bg-base-content/15"
+            />
             <div class="flex flex-col">
               <span class="break-all text-lg font-semibold text-info"
                 >q/{introduction_data.name || 'communityname'}</span
