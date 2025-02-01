2f963730414c2462d87a4e3a6eb6c3ce6e58d78f
ilovemoon <moonlitgrace.gaia@gmail.com>
2025-02-01 17:36:00 +0530

    feat: integrate superforms in community creation modal (#158)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/src/lib/modals/modal-create-community/forms/introduction.svelte
+++ frontend/src/lib/modals/modal-create-community/forms/introduction.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import autosize from '$lib/actions/autosize';
   import Avatar from '$lib/components/ui/avatar.svelte';
-  import ZodErrors from '$lib/components/zod-errors.svelte';
   import type { FormProps } from '../../types';
   import forms from '../forms';
+  import { defaults, superForm } from 'sveltekit-superforms';
+  import { zod } from 'sveltekit-superforms/adapters';
   import { z } from 'zod';
 
   const schema = z.object({
@@ -11,13 +12,14 @@
       .string()
       .min(3)
       .regex(/^[a-zA-Z0-9_]+$/, { message: 'Only letters, numbers, and underscores are allowed' }),
-    description: z.string().min(1)
+    description: z.string().min(1),
+    avatar: z.string().optional(),
+    banner: z.string().optional()
   });
 
-  let { update_forms_state, forms_state }: FormProps<typeof forms> = $props();
+  let { forms_state, update_forms_state }: FormProps<typeof forms> = $props();
 
-  let errors = $state<z.ZodIssue[]>();
-  const introduction_data = $state({
+  const initial_data = {
     ...(
       forms_state.introduction as {
         data: Partial<{
@@ -28,32 +30,46 @@
         }>;
       }
     ).data
-  });
+  };
+
+  const { form, enhance, errors, validate, validateForm } = superForm(
+    defaults(initial_data, zod(schema)),
+    {
+      SPA: true,
+      resetForm: false,
+      validators: zod(schema),
+      onChange: async () => {
+        // TODO: call external API to check for name availability
+
+        const result = await validateForm({ update: false });
+        update_forms_state('introduction', {
+          valid: result.valid,
+          data: { ...result.data }
+        });
+      }
+    }
+  );
 
   function handle_file_change(e: Event, type: 'avatar' | 'banner') {
     const file = (e.target as HTMLInputElement).files?.[0];
     if (!file) return;
 
+    // convert image file to data uri
     const reader = new FileReader();
     reader.onload = () => {
-      if (type === 'avatar') introduction_data.avatar = String(reader.result);
-      if (type === 'banner') introduction_data.banner = String(reader.result);
+      $form[type] = String(reader.result);
     };
     reader.readAsDataURL(file);
   }
 
-  function handle_input() {
-    const result = schema.safeParse({
-      name: introduction_data.name ?? '',
-      description: introduction_data.description ?? ''
-    });
+  function handle_name_input(e: Event) {
+    // remove unncessarry characters
+    $form.name = (e.currentTarget as HTMLInputElement).value.replace(/\s/g, '');
+    validate('name');
+  }
 
-    errors = result.error?.errors;
-
-    update_forms_state('introduction', {
-      valid: !errors,
-      data: { ...introduction_data }
-    });
+  function handle_description_input() {
+    validate('description');
   }
 </script>
 
@@ -67,53 +83,65 @@
   </div>
 
   <!-- form section -->
-  <form class="flex flex-col-reverse items-start gap-2 md:flex-row md:gap-6">
+  <form use:enhance class="flex flex-col-reverse items-start gap-2 md:flex-row md:gap-6">
     <!-- left column: input fields -->
     <div class="flex w-full flex-1 flex-col gap-2">
       <!-- name input -->
       <label class="form-control">
         <div class="label py-1">
           <span class="label-text">Name*</span>
-          <span class="label-text-alt">{introduction_data.name?.length ?? 0}/25</span>
+          <span class="label-text-alt">{$form.name.length}/25</span>
         </div>
         <label
           class="input input-bordered flex h-10 items-center gap-2 bg-transparent text-sm font-medium"
         >
           q/
           <input
-            bind:value={introduction_data.name}
             type="text"
             class="grow border-none p-0 text-sm placeholder:font-normal placeholder:opacity-50 focus:ring-0"
             placeholder="eg: quibble"
             maxlength={25}
-            oninput={(e) => {
-              introduction_data.name = e.currentTarget.value.replace(/\s/g, '');
-              handle_input();
-            }}
+            bind:value={$form.name}
+            oninput={handle_name_input}
           />
         </label>
+
+        <!-- error store -->
+        {#if $errors.name}
+          <div class="label py-1">
+            <span class="label-text flex items-center gap-2 text-error">
+              <coreicons-shape-x variant="circle" class="size-3.5"></coreicons-shape-x>
+              <span class="text-xs">{$errors.name[0]}</span>
+            </span>
+          </div>
+        {/if}
       </label>
 
       <!-- description input -->
       <label class="form-control">
         <div class="label py-1">
           <span class="label-text">Description*</span>
-          <span class="label-text-alt">{introduction_data.description?.length ?? 0}/255</span>
+          <span class="label-text-alt">{$form.description.length}/255</span>
         </div>
         <textarea
-          bind:value={introduction_data.description}
           use:autosize
           class="textarea textarea-bordered max-h-[15rem] min-h-[7.5rem] bg-transparent leading-normal placeholder:opacity-50"
           placeholder="Tell something nice about your community..."
           maxlength={255}
-          oninput={handle_input}
+          bind:value={$form.description}
+          oninput={handle_description_input}
         ></textarea>
-      </label>
 
-      <!-- display zod errors -->
-      {#if errors}
-        <ZodErrors {errors} />
-      {/if}
+        <!-- error store -->
+        {#if $errors.description}
+          <div class="label py-1">
+            <span class="label-text flex items-center gap-2 text-error">
+              <coreicons-shape-x variant="circle" class="size-3.5"></coreicons-shape-x>
+              <span class="text-xs">{$errors.description[0]}</span>
+            </span>
+          </div>
+        {/if}
+      </label>
     </div>
 
     <!-- right column: avatar and banner -->
@@ -146,17 +174,17 @@
       <div class="overflow-hidden rounded-2xl bg-base-100">
         <div
           class="flex h-10 bg-info bg-cover bg-center"
-          style="background-image: url({introduction_data.banner});"
+          style="background-image: url({$form.banner});"
         ></div>
         <div class="flex flex-col gap-4 p-4">
           <div class="flex items-center gap-4">
             <Avatar
-              src={introduction_data.avatar}
+              src={$form.avatar}
               class="size-12 flex-shrink-0 !bg-base-content/15 md:size-14"
             />
             <div class="flex flex-col">
               <span class="break-all text-base font-semibold text-info md:text-lg">
-                q/{introduction_data.name || 'communityname'}
+                q/{$form.name || 'communityname'}
               </span>
               <div class="flex items-center gap-2">
                 <span class="text-xs">1 member</span>
@@ -166,7 +194,7 @@
             </div>
           </div>
           <p class="whitespace-pre-line break-words text-sm">
-            {introduction_data.description || 'Community description'}
+            {$form.description || 'Community description'}
           </p>
         </div>
       </div>
