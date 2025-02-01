fd7eec72f390e464a07a58e4aa441cc232be3db8
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-02-01 11:26:04 +0530

    feat: add superforms instance on client done validations

--- frontend/src/lib/modals/modal-create-community/forms/introduction.svelte
+++ frontend/src/lib/modals/modal-create-community/forms/introduction.svelte
@@ -4,6 +4,8 @@
   import ZodErrors from '$lib/components/zod-errors.svelte';
   import type { FormProps } from '../../types';
   import forms from '../forms';
+  import { defaults, superForm } from 'sveltekit-superforms';
+  import { zod } from 'sveltekit-superforms/adapters';
   import { z } from 'zod';
 
   const schema = z.object({
@@ -16,7 +18,21 @@
 
   let { update_forms_state, forms_state }: FormProps<typeof forms> = $props();
 
-  let errors = $state<z.ZodIssue[]>();
+  const { form, enhance, errors, validate } = superForm(defaults(zod(schema)), {
+    SPA: true,
+    resetForm: false,
+    validators: zod(schema),
+    onUpdate({ form }) {
+      console.log(form);
+      if (form.valid) {
+        // call an external API with form.data, await the result and update form
+      }
+    },
+    onChange({ get }) {
+      console.log('get', get('name'));
+    }
+  });
+
   const introduction_data = $state({
     ...(
       forms_state.introduction as {
@@ -34,6 +50,7 @@
     const file = (e.target as HTMLInputElement).files?.[0];
     if (!file) return;
 
+    // convert image file to data uri
     const reader = new FileReader();
     reader.onload = () => {
       if (type === 'avatar') introduction_data.avatar = String(reader.result);
@@ -42,18 +59,14 @@
     reader.readAsDataURL(file);
   }
 
-  function handle_input() {
-    const result = schema.safeParse({
-      name: introduction_data.name ?? '',
-      description: introduction_data.description ?? ''
-    });
+  function handle_name_input(e: Event) {
+    // remove unncessarry characters
+    $form.name = (e.currentTarget as HTMLInputElement).value.replace(/\s/g, '');
+    validate('name');
+  }
 
-    errors = result.error?.errors;
-
-    update_forms_state('introduction', {
-      valid: !errors,
-      data: { ...introduction_data }
-    });
+  function handle_description_input() {
+    validate('description');
   }
 </script>
 
@@ -67,7 +80,7 @@
   </div>
 
   <!-- form section -->
-  <form class="flex flex-col-reverse items-start gap-2 md:flex-row md:gap-6">
+  <form use:enhance class="flex flex-col-reverse items-start gap-2 md:flex-row md:gap-6">
     <!-- left column: input fields -->
     <div class="flex w-full flex-1 flex-col gap-2">
       <!-- name input -->
@@ -81,17 +94,24 @@
         >
           q/
           <input
-            bind:value={introduction_data.name}
             type="text"
             class="grow border-none p-0 text-sm placeholder:font-normal placeholder:opacity-50 focus:ring-0"
             placeholder="eg: quibble"
             maxlength={25}
-            oninput={(e) => {
-              introduction_data.name = e.currentTarget.value.replace(/\s/g, '');
-              handle_input();
-            }}
+            bind:value={$form.name}
+            oninput={handle_name_input}
           />
         </label>
+
+        <!-- error store -->
+        {#if $errors.name}
+          <div class="label py-1">
+            <span class="label-text flex items-center gap-2 text-error">
+              <coreicons-shape-x variant="circle" class="size-3.5"></coreicons-shape-x>
+              <span class="text-xs">{$errors.name[0]}</span>
+            </span>
+          </div>
+        {/if}
       </label>
 
       <!-- description input -->
@@ -101,19 +121,24 @@
           <span class="label-text-alt">{introduction_data.description?.length ?? 0}/255</span>
         </div>
         <textarea
-          bind:value={introduction_data.description}
           use:autosize
           class="textarea textarea-bordered max-h-[15rem] min-h-[7.5rem] bg-transparent leading-normal placeholder:opacity-50"
           placeholder="Tell something nice about your community..."
           maxlength={255}
-          oninput={handle_input}
+          bind:value={$form.description}
+          oninput={handle_description_input}
         ></textarea>
-      </label>
 
-      <!-- display zod errors -->
-      {#if errors}
-        <ZodErrors {errors} />
-      {/if}
+        <!-- error store -->
+        {#if $errors.description}
+          <div class="label py-1">
+            <span class="label-text flex items-center gap-2 text-error">
+              <coreicons-shape-x variant="circle" class="size-3.5"></coreicons-shape-x>
+              <span class="text-xs">{$errors.description}</span>
+            </span>
+          </div>
+        {/if}
+      </label>
     </div>
 
     <!-- right column: avatar and banner -->
