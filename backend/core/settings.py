41fb09fb95d5e944a8bc8e9ef523327d306429e2
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-02-02 22:51:48 +0530

    fix: api send request path issue

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -101,7 +101,8 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '0.0.1',
     'SERVE_INCLUDE_SCHEMA': False,
     'SCHEMA_PATH_PREFIX': r'/api/v[1-9]/',
-    # 'SCHEMA_PATH_PREFIX_TRIM': True,
+    'SCHEMA_PATH_PREFIX_TRIM': True,
+    'SERVERS': [{'url': '/api/v1', 'description': 'v1 API version'}],
     # sidecar config
     'SWAGGER_UI_DIST': 'SIDECAR',
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
