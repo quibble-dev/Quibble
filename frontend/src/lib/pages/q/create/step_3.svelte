32e2f6bddcb81eefb2018dac3dd1d6e280dca6ab
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-02-25 18:56:50 +0530

    feat: integrate search filter

--- frontend/src/lib/pages/q/create/step_3.svelte
+++ frontend/src/lib/pages/q/create/step_3.svelte
@@ -2,27 +2,69 @@
   import client from '$lib/clients/v1/client';
   import type { components } from '$lib/clients/v1/schema';
   import { cn } from '$lib/functions/classnames';
+  import { onMount } from 'svelte';
 
   type Topic = components['schemas']['Topic'];
 
+  const SELECTED_TOPIC_LIMIT = 3;
+
+  let pending = $state(false);
+  let filter_input_value = $state('');
+
   let selected_topics = $state<Topic[]>([]);
+  let topics_raw = $state<Topic[]>([]);
+  let topics = $derived.by<Topic[]>(() => {
+    const value = filter_input_value.toLowerCase();
+    // https://stackoverflow.com/a/41543705/26860113
+    const emoji_regex = /([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g;
+
+    function normalize_str(str: string) {
+      return str.replace(emoji_regex, '').trim().toLowerCase();
+    }
+
+    function filter_str(str: string) {
+      return normalize_str(str)
+        .split(' ')
+        .some((s) => s.startsWith(value));
+    }
+
+    return topics_raw.filter(
+      (topic) =>
+        filter_str(topic.display_name) ||
+        (topic.children as unknown as Topic[]).some((t) => filter_str(t.display_name))
+    );
+  });
 
   async function fetch_topics() {
-    const { data, response } = await client.GET('/q/topics/');
-    if (response.ok && data) {
-      return data;
+    try {
+      pending = true;
+
+      const { data, response } = await client.GET('/q/topics/');
+      if (response.ok && data) {
+        topics_raw = data;
+      }
+    } finally {
+      pending = false;
     }
+  }
+
+  function check_is_selected(id: number): boolean {
+    return selected_topics.some((t) => t.id === id);
   }
 
   function handle_toggle_select_topic(topic: Topic) {
     // remove topic if already selected
-    if (selected_topics.includes(topic)) {
-      selected_topics = selected_topics.filter((t) => t !== topic);
+    if (check_is_selected(topic.id)) {
+      selected_topics = selected_topics.filter((t) => t.id !== topic.id);
     } else {
-      if (selected_topics.length >= 3) return;
+      if (selected_topics.length >= SELECTED_TOPIC_LIMIT) return;
       selected_topics = [...selected_topics, topic];
     }
   }
+
+  onMount(() => {
+    fetch_topics();
+  });
 </script>
 
 <div class="flex flex-col gap-2">
@@ -32,6 +74,7 @@
       type="text"
       class="grow border-none px-2 text-sm font-medium placeholder:opacity-50 focus:ring-0"
       placeholder="Filter topics..."
+      bind:value={filter_input_value}
     />
     <button type="button" class="btn btn-square btn-ghost btn-xs" aria-label="clear topic filters">
       <coreicons-shape-x class="size-4" variant="no-border"></coreicons-shape-x>
@@ -41,36 +84,39 @@
     <span class="text-sm font-medium">Topics {selected_topics.length}/3:</span>
     {#each selected_topics as topic}
       <button
+        type="button"
         class="btn btn-ghost btn-active btn-xs md:btn-sm"
         onclick={() => handle_toggle_select_topic(topic)}
       >
-        {topic}
+        {topic.display_name}
         <coreicons-shape-x variant="circle" class="size-4"></coreicons-shape-x>
       </button>
     {/each}
   </div>
 </div>
 <div class="flex max-h-80 flex-col gap-2 overflow-y-scroll pr-2">
-  {#await fetch_topics()}
+  {#if pending}
     <div class="grid place-items-center">
       <span class="loading loading-dots loading-md"></span>
       <span class="text-sm">Fetching topics...</span>
     </div>
-  {:then topics}
-    {#each topics ?? [] as topic}
+  {:else}
+    {#each topics as topic (topic.id)}
       <div class="flex flex-col gap-2">
-        <span class="text-sm font-medium">{topic.display_name}</span>
+        <span class="text-sm font-medium">{topic.icon} {topic.display_name}</span>
         <div class="flex flex-wrap items-center gap-2">
-          {#each topic.children as unknown as Topic[] as sub_topic}
-            {@const is_selected = selected_topics.includes(sub_topic)}
+          {#each topic.children as unknown as Topic[] as t (t.id)}
+            {@const is_selected = check_is_selected(t.id)}
+
             <button
-              onclick={() => handle_toggle_select_topic(sub_topic)}
+              type="button"
+              onclick={() => handle_toggle_select_topic(t)}
               class={cn(
-                is_selected ? 'btn-info' : 'bg-base-content/10 hover:btn-ghost',
+                is_selected ? 'btn-ghost btn-active' : 'btn-neutral',
                 'btn btn-xs border-none md:btn-sm'
               )}
             >
-              <span>{sub_topic.icon} {sub_topic.display_name}</span>
+              <span>{t.icon} {t.display_name}</span>
               {#if is_selected}
                 <coreicons-shape-x variant="circle" class="size-4"></coreicons-shape-x>
               {/if}
@@ -79,5 +125,5 @@
         </div>
       </div>
     {/each}
-  {/await}
+  {/if}
 </div>
