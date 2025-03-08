d26460626803f4de802d215a829ba4ee8557d035
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-03-08 08:11:42 +0530

    chore: revert box layout change on #9b90f6f

--- frontend/src/routes/(auth)/+layout.svelte
+++ frontend/src/routes/(auth)/+layout.svelte
@@ -32,7 +32,7 @@
   <div
     class="absolute inset-0 bg-base-100 [mask-image:url('/assets/svgs/standalone-auth-bg.svg')] [mask-size:cover]"
   ></div>
-  <div class="relative flex flex-col gap-5">
+  <div class="relative flex flex-col gap-5 rounded-box bg-base-300 p-6 md:w-[25rem]">
     <!-- header section -->
     <div class="flex flex-col items-center justify-center gap-2">
       <a href="/" class="flex items-center gap-2">
@@ -58,48 +58,46 @@
         {/if}
       </span>
     </div>
-    <div class="flex flex-col gap-4 rounded-box bg-base-300 p-6 md:w-[25rem]">
-      {#if render_type === 'select'}
-        <ProfileSelect
-          onclick={(type) => {
-            if (type === 'back') render_type = null;
-            else if (type === 'create') render_type = 'create';
-          }}
-        />
-      {:else if render_type === 'create'}
-        <ProfileCreate
-          onback={() => (render_type = 'select')}
-          onsuccess={() => (render_type = 'select')}
-        />
-      {:else if render_type === 'code'}
-        <Code email={data?.email} onback={() => (render_type = null)} />
-      {:else}
-        <!-- oauth section -->
-        <div class="flex w-full items-center gap-3">
-          <!-- oauth section: google -->
-          <button class="btn flex-1">
-            <GoogleLogo class="size-5" />
-            Google
-          </button>
-          <!-- oauth section: github -->
-          <button class="btn flex-1">
-            <coreicons-logo-github class="size-5"></coreicons-logo-github>
-            Github
-          </button>
-        </div>
-        <div class="divider my-0 text-xs font-bold">OR</div>
-        <!-- rest pages -->
-        {@render children()}
-        <!-- footer section -->
-        <p class="text-center text-xs">
-          By continuing, you agree to the <a
-            href="/support/terms-and-conditions"
-            class="font-medium text-info underline">Terms of use</a
-          >,
-          <a href="/support/privary" class="font-medium text-info underline">Privacy</a>
-          and <a href="/support/policy" class="font-medium text-info underline">Policy</a> Preplaced.
-        </p>
-      {/if}
-    </div>
+    {#if render_type === 'select'}
+      <ProfileSelect
+        onclick={(type) => {
+          if (type === 'back') render_type = null;
+          else if (type === 'create') render_type = 'create';
+        }}
+      />
+    {:else if render_type === 'create'}
+      <ProfileCreate
+        onback={() => (render_type = 'select')}
+        onsuccess={() => (render_type = 'select')}
+      />
+    {:else if render_type === 'code'}
+      <Code email={data?.email} onback={() => (render_type = null)} />
+    {:else}
+      <!-- oauth section -->
+      <div class="flex w-full items-center gap-3">
+        <!-- oauth section: google -->
+        <button class="btn flex-1">
+          <GoogleLogo class="size-5" />
+          Google
+        </button>
+        <!-- oauth section: github -->
+        <button class="btn flex-1">
+          <coreicons-logo-github class="size-5"></coreicons-logo-github>
+          Github
+        </button>
+      </div>
+      <div class="divider my-0 text-xs font-bold">OR</div>
+      <!-- rest pages -->
+      {@render children()}
+      <!-- footer section -->
+      <p class="text-center text-xs">
+        By continuing, you agree to the <a
+          href="/support/terms-and-conditions"
+          class="font-medium text-info underline">Terms of use</a
+        >,
+        <a href="/support/privary" class="font-medium text-info underline">Privacy</a>
+        and <a href="/support/policy" class="font-medium text-info underline">Policy</a> Preplaced.
+      </p>
+    {/if}
   </div>
 </div>
