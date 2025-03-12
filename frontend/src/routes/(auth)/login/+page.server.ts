41b4c2b6d952bd90fd8c9c84fabf953c42f84a41
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-03-12 17:31:23 +0530

    fix: eslint error;

--- frontend/src/routes/(auth)/login/+page.server.ts
+++ frontend/src/routes/(auth)/login/+page.server.ts
@@ -4,7 +4,7 @@ import { LoginSchema, ProfileCreateSchema } from '$lib/schemas/auth';
 import { set_cookies_from_header } from '$lib/server/utils/cookie';
 import type { PageServerLoad } from './$types';
 import { fail, type Actions } from '@sveltejs/kit';
-import { message, setError, superValidate, withFiles } from 'sveltekit-superforms';
+import { message, superValidate, withFiles } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 export const load: PageServerLoad = async ({ url }) => {
