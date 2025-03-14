bafbc045c47763c443981aeb1ed37e6d7bed2e87
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-03-14 09:49:04 +0530

    refactor: lib/schemas

--- frontend/src/lib/schemas/auth.ts
+++ frontend/src/lib/schemas/auth.ts
@@ -1,27 +1,27 @@
 import { z } from 'zod';
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8)
+  email: z.string().email('Please enter a valid email address.'),
+  password: z.string().min(8, 'Password must be at least 8 characters long.')
 });
 
 export const RegisterSchema = z
   .object({
-    email: z.string().email(),
-    password1: z.string().min(8),
-    password2: z.string().min(8)
+    email: z.string().email('Please enter a valid email address.'),
+    password1: z.string().min(8, 'Password must be at least 8 characters long.'),
+    password2: z.string().min(8, 'Password must be at least 8 characters long.')
   })
   .refine((data) => data.password1 === data.password2, {
-    message: `Passwords don't match`,
+    message: `Passwords don't match.`,
     path: ['password2']
   });
 
 export const VerificationCodeSchema = z.object({
-  code: z.string().min(1).max(6)
+  code: z.string().min(1, 'Verification code is required.').max(6, 'Invalid verification code.')
 });
 
 export const ProfileCreateSchema = z.object({
-  username: z.string().min(3),
+  username: z.string().min(3, 'Username must be at least 3 characters long.'),
   avatar: z
     .instanceof(File)
     .refine((f) => f.size < 5_000_000, 'Max 5 MB upload size.')
--- frontend/src/lib/schemas/community-create.ts
+++ frontend/src/lib/schemas/community-create.ts
@@ -2,8 +2,8 @@ import type { SuperFormData, SuperFormErrors } from 'sveltekit-superforms/client
 import { z } from 'zod';
 
 export const CommunityCreateSchema = z.object({
-  name: z.string().min(1),
-  description: z.string().min(1),
+  name: z.string().min(1, 'Community name is required.'),
+  description: z.string().min(1, 'Description cannot be empty.'),
   avatar: z
     .instanceof(File)
     .refine((f) => f.size < 5_000_000, 'Max 5 MB upload size.')
@@ -12,7 +12,7 @@ export const CommunityCreateSchema = z.object({
     .instanceof(File)
     .refine((f) => f.size < 5_000_000, 'Max 5 MB upload size.')
     .optional(),
-  topics: z.number().array().min(1),
+  topics: z.number().array().min(1, 'Please select at least one topic.'),
   type: z.enum(['PRIVATE', 'RESTRICTED', 'PUBLIC']).default('PUBLIC'),
   nsfw: z.boolean().default(false)
 });
