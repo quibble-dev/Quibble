24480bc6a01bea323f9dae97330d992c6062db45
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-03-21 10:46:26 +0530

    fix: send proper cookies in request

--- frontend/src/routes/callback/+page.server.ts
+++ frontend/src/routes/callback/+page.server.ts
@@ -15,8 +15,16 @@ export const load: PageServerLoad = async ({ request, url, cookies }) => {
       const set_cookie_header = response.headers.getSetCookie();
       set_cookies_from_header(set_cookie_header, cookies);
 
+      // manually construct cookie for next request
+      const cookies_to_send = cookies
+        .getAll()
+        .map((cookie) => `${cookie.name}=${cookie.value}`)
+        .join('; ');
+
       // check profiles
-      const { data } = await api.GET('/u/me/profiles/total-count/');
+      const { data } = await api.GET('/u/me/profiles/total-count/', {
+        headers: { Cookie: cookies_to_send }
+      });
       const type = data && data.total_count > 0 ? 'select' : 'create';
 
       redirect(303, `/login?type=${type}&ref=callback`);
