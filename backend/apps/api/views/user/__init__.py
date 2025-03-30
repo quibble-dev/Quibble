18c64c2ad9beed3a48c5b7e1ea7c7cf961640d71
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-03-30 21:51:50 +0530

    feat: delete account endpoint and modal

--- backend/apps/api/views/user/__init__.py
+++ backend/apps/api/views/user/__init__.py
@@ -1,27 +1,56 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import exceptions, permissions, views
+from rest_framework import permissions, status, views
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
-from ...serializers.user.profile import ProfileSerializer
+from apps.api.serializers.user import UserSerializer
+from apps.api.serializers.user.profile import ProfileSerializer
+from apps.api.utils import unset_jwt_cookies_with_profile_id
 
 
 @extend_schema(tags=['user & profiles'])
 class MeAPIView(views.APIView):
-    """
-    View to retrieve information for the currently authenticated user.
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = UserSerializer
 
-    - `get`: Returns the details of the authenticated user based on their token.
+    def get(self, request):
+        serializer = self.serializer_class(request.user, context={'request': request})
+        return Response(serializer.data)
 
-    Permission:
-    - Requires user authentication.
-    """
+    def delete(self, request):
+        try:
+            request.user.delete()
+            response = Response(
+                {'detail': 'User account deleted successfully!'}, status=status.HTTP_204_NO_CONTENT
+            )
+            unset_jwt_cookies_with_profile_id(response)
 
+            return response
+        except Exception as e:
+            raise ValidationError(f'An unexpected error occured: {str(e)}')
+
+
+@extend_schema(tags=['user & profiles'])
+class MeProfileAPIView(views.APIView):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ProfileSerializer
 
     def get(self, request):
-        if request.user_profile:
-            serializer = self.serializer_class(request.user_profile, context={'request': request})
-            return Response(serializer.data)
-        else:
-            raise exceptions.ValidationError('A valid profile must be provided.')
+        serializer = self.serializer_class(request.user_profile, context={'request': request})
+        return Response(serializer.data)
+
+    def delete(self, request):
+        user_profile = request.user_profile
+        if user_profile is None:
+            raise ValidationError('No profile is selected for deletion.')
+
+        try:
+            user_profile.delete()
+            response = Response(
+                {'detail': 'Profile deleted successfully!'}, status=status.HTTP_204_NO_CONTENT
+            )
+            unset_jwt_cookies_with_profile_id(response)
+
+            return response
+        except Exception as e:
+            raise ValidationError(f'An unexpected error occured: {str(e)}')
--- frontend/src/routes/(app)/settings/+page.svelte
+++ frontend/src/routes/(app)/settings/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+  import { goto, invalidateAll } from '$app/navigation';
+  import api from '$lib/api';
+  import BaseModal from '$lib/components/ui/base-modal.svelte';
+  import { cn } from '$lib/functions/classnames';
   import type { ISettingItem } from '$lib/pages/settings/setting-item.svelte';
   import SettingItem from '$lib/pages/settings/setting-item.svelte';
   import { auth_store } from '$lib/stores/auth.svelte';
+  import type { Nullable } from '$lib/types/shared';
 
   const general_settings: ISettingItem[] = $derived([
     {
@@ -33,6 +38,27 @@
       is_dangerous: true
     }
   ];
+
+  let pending = $state(false);
+  let show_delete_modal = $state(false);
+
+  async function handle_delete() {
+    // eslint-disable-next-line no-undef
+    let pending_timeout: Nullable<NodeJS.Timeout> = null;
+    try {
+      pending_timeout = setTimeout(() => (pending = true), 500);
+      const { response } = await api.DELETE('/u/me/');
+      if (response.ok) {
+        await invalidateAll();
+        await goto('/');
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      pending = false;
+      if (pending_timeout) clearTimeout(pending_timeout);
+    }
+  }
 </script>
 
 <svelte:head>
@@ -49,6 +75,38 @@
   <h3 class="text-lg font-medium">Advanced</h3>
   <div class="divider"></div>
   {#each advanced_settings as setting}
-    <SettingItem {...setting} />
+    <SettingItem
+      {...setting}
+      onclick={() => {
+        if (setting.is_dangerous) show_delete_modal = true;
+      }}
+    />
   {/each}
 </div>
+
+<BaseModal
+  open={show_delete_modal}
+  onclose={() => (show_delete_modal = false)}
+  class="flex max-w-[25rem]! flex-col gap-4"
+>
+  <div class="flex flex-col">
+    <h3 class="text-info font-medium">Delete account</h3>
+    <span class="text-base-content/75 text-sm">
+      Deleting your account will also delete all profiles associated with it.
+    </span>
+  </div>
+  <div class="flex items-center justify-end gap-2">
+    <button type="button" onclick={() => (show_delete_modal = false)} class="btn">Cancel</button>
+    <button
+      class={cn(pending && 'btn-active pointer-events-none', 'btn btn-error')}
+      onclick={handle_delete}
+    >
+      Delete
+      {#if pending}
+        <span class="loading loading-spinner loading-xs"></span>
+      {:else}
+        <coreicons-shape-trash variant="without-lines" class="size-4"></coreicons-shape-trash>
+      {/if}
+    </button>
+  </div>
+</BaseModal>
