86ca0b3fb67c4a1bbb8c4d3578008a110d5a5c81
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-04-05 17:00:15 +0530

    Revert "chore: proper vercel analytics injection"
    This reverts commit d759ec2fbb012d448a06abe7e885ac7c50427d0d.

--- frontend/src/routes/+layout.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment';
-import { injectAnalytics } from '@vercel/analytics/sveltekit';
-
-// vercel analytics
-injectAnalytics({ mode: dev ? 'development' : 'production' });
