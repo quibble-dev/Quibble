ff662fc326da824a0536c8d66df6a37bd2106528
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-04-10 08:05:30 +0530

    fixes: #303; map backend errors to superforms

--- frontend/src/routes/(app)/submit/+page.server.ts
+++ frontend/src/routes/(app)/submit/+page.server.ts
@@ -3,7 +3,7 @@ import { create_form_data, type FormDataObject } from '$lib/functions/form';
 import { PostSubmitSchema } from '$lib/schemas/post-submit';
 import type { PageServerLoad } from '../$types';
 import { fail, redirect, type Actions } from '@sveltejs/kit';
-import { superValidate, withFiles } from 'sveltekit-superforms';
+import { setError, superValidate, withFiles } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 export const load: PageServerLoad = async () => {
@@ -20,7 +20,7 @@ export const actions: Actions = {
       return fail(400, withFiles({ form }));
     }
 
-    const { data, response } = await api.POST('/posts/', {
+    const { data, response, error } = await api.POST('/posts/', {
       headers: { Cookie: request.headers.get('Cookie') },
       // @ts-expect-error: needs cover field in File type
       body: {
@@ -32,8 +32,14 @@ export const actions: Actions = {
       }
     });
 
-    if (response.ok && data)
+    if (response.ok && data) {
       redirect(307, `/q/${data.community.name}/posts/${data.id}/${data.slug}`);
+    } else if (error) {
+      const err = error.errors[0];
+      // @ts-expect-error: because of dynamic attr
+      return setError(form, err?.attr, err?.detail);
+    }
+
     return withFiles({ form });
   }
 };
